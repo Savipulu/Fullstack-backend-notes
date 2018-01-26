d0d83e6baa82b6198fa36da6892d82f71ba74461
Savipulu <jouni.winter@gmail.com>
2018-01-26 20:27:56 +0200

    /notes -> api/notes

--- index.js
+++ index.js
@@ -37,15 +37,15 @@ let notes = [
   }
 ];
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/notes/:id", (req, res) => {
+app.get("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   const note = notes.find(note => note.id === id);
 
@@ -56,7 +56,7 @@ app.get("/notes/:id", (req, res) => {
   }
 });
 
-app.delete("/notes/:id", (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   notes = notes.filter(note => note.id !== id);
 
@@ -74,7 +74,7 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post("/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
   const body = req.body;
 
   if (body.content === undefined) {
